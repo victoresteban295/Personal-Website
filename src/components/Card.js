5ede23f2926fe7029f6531949bf87adfc1a0c121
victoresteban295 <esteban.victor295@gmail.com>
2022-08-04 14:20:50 -0700

    Added some effects to buttons & started react-pdf for resume

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Document, Page } from "react-pdf";
+import Resume from "../images/pdf02.pdf"
 
 export default function Card() {
+
+    const [numPages, setNumPages] = React.useState(null);
+    const [pageNum, setPageNum] = React.useState(1);
+
+    function onDocumentLoadSuccess(numPages) {
+        setNumPages(numPages);
+    }
+
     return (
         <div className="card-container">
-
+            <Document file={Resume} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNum}/>
+            </Document>
         </div>
     )
 }
