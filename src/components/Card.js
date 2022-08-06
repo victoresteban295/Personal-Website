46776290ba556f956f00ac7c0cf9633137a79c9d
victoresteban295 <esteban.victor295@gmail.com>
2022-08-06 10:58:52 -0700

    installed pdf-react & added resume

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { Document, Page } from "react-pdf";
-import Resume from "../images/pdf02.pdf"
+import { Document, Page, pdfjs } from "react-pdf";
+import Resume from "../images/Resume_Example.pdf"
 
 export default function Card() {
 
     const [numPages, setNumPages] = React.useState(null);
     const [pageNum, setPageNum] = React.useState(1);
 
+    pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
+
     function onDocumentLoadSuccess(numPages) {
         setNumPages(numPages);
     }
 
     return (
-        <div className="card-container">
-            <Document file={Resume} onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNum}/>
-            </Document>
+        <div className="card-container card-resume">
+            
+                <Document 
+                    file={Resume} 
+                    onLoadSuccess={onDocumentLoadSuccess}
+                    className="resume-document"
+                    renderMode="svg"
+                >
+                    <Page 
+                        pageNumber={pageNum} 
+                        renderMode="svg" 
+                        className="resume-page-container"
+                        height={950}
+                    />
+                </Document>
+            
         </div>
     )
 }
