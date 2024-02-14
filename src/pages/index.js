58be72e73e32c1429e7d025cd36d4c0ce40c70a6
victoresteban295 <esteban.victor295@gmail.com>
2024-02-13 16:13:50 -0800

    Added some metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Footer from "../components/Footer";
 import ProjectsSection from "../components/projects/ProjectsSection"
 import ResumeSection from "../components/resume/ResumeSection"
 import NavBar from "../components/Navbar";
+import Head from "next/head";
 
 const HomePage = ({ 
     isDarkMode, 
@@ -16,46 +17,51 @@ const HomePage = ({
     isResumeVisible }) => {
 
     return (
-        <Box
-            id="page"
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                backgroundColor: 'primary.main',
-                color: 'text.primary',
-                overflowY: 'auto',
-                flexGrow: 1,
-            }}
-        >
-            <NavBar 
-                isDarkMode={isDarkMode}
-                toggleDarkMode={toggleDarkMode}
-                isHomeVisible={isHomeVisible}
-                isProjectsVisible={isProjectsVisible}
-                isResumeVisible={isResumeVisible}
-            />
+        <>
+            <Head>
+                <title>{"Victor Esteban's Portfolio"}</title>
+            </Head>
             <Box
-                id="home"
+                id="page"
                 sx={{
                     display: 'flex',
                     flexDirection: 'column',
-                    alignItems: 'center',
-                    width: '100%',
+                    backgroundColor: 'primary.main',
+                    color: 'text.primary',
+                    overflowY: 'auto',
+                    flexGrow: 1,
                 }}
             >
-                <HomeSection 
+                <NavBar 
                     isDarkMode={isDarkMode}
-                    homeRef={homeRef}
+                    toggleDarkMode={toggleDarkMode}
+                    isHomeVisible={isHomeVisible}
+                    isProjectsVisible={isProjectsVisible}
+                    isResumeVisible={isResumeVisible}
                 />
-                <ProjectsSection 
-                    projectsRef={projectsRef}
-                />
-                <ResumeSection 
-                    resumeRef={resumeRef}
-                />
-                <Footer />
+                <Box
+                    id="home"
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        width: '100%',
+                    }}
+                >
+                    <HomeSection 
+                        isDarkMode={isDarkMode}
+                        homeRef={homeRef}
+                    />
+                    <ProjectsSection 
+                        projectsRef={projectsRef}
+                    />
+                    <ResumeSection 
+                        resumeRef={resumeRef}
+                    />
+                    <Footer />
+                </Box>
             </Box>
-        </Box>
+        </>
     )
 }
 
